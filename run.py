f67b193513e0eee19df3a6107b1aaa4ef53a7533
Stephanie Fung <sfung@slac.stanford.edu>
2024-03-26 15:21:26 -0700

    IP addr and readout updated for new scope in UND

--- run.py
+++ run.py
@@ -4,23 +4,26 @@ import time
 from epics import caget, caput, cainfo
 
 
-visa_address = 'TCPIP::172.21.161.49::INSTR'
+visa_address = 'TCPIP::172.21.161.39::INSTR'
 
 # rm = pyvisa.ResourceManager()
 #print(rm.list_resources())
 # scope = rm.open_resource(visa_address)
 instr =  vxi11.Instrument(visa_address)
 
+## Measurement setup 
+# instr.write('CH1:SCALE 2.0E-1')
+# instr.write('CH2:SCALE 2.0E-1')
+# instr.write('CH3:SCALE 2.0E-1')
+# instr.write('MEASUREMENT:MEAS3:STATE ON')
+# instr.write('MEASUREMENT:MEAS3:TYPE DELAY CH1:CH2')
+# instr.write('MEASUREMENT:MEAS4:TYPE DELAY CH1:CH3')
 
 run = 1
-
 while run == 1:
-
     caput("LAS:CAST:FLOAT:01",instr.ask('MEASUREMENT:MEAS1:VALUE?')) #QUERY SCOPE FOR MEAS 1 - PHASE DIFFERENCE BETWEEN S29 - NEH
-    caput("LAS:CAST:FLOAT:02",instr.ask('MEASUREMENT:MEAS2:VALUE?')) #QUERY SCOPE FOR MEAS 2 - PHASE DIFFERENCE BETWEEN S29 - FEH
-    caput("LAS:CAST:FLOAT:03",instr.ask('MEASUREMENT:MEAS3:VALUE?')) #QUERY SCOPE FOR MEAS 3 - AMPLITUDE OF S29 REF RF, ASSIGN TO PV
-    caput("LAS:CAST:FLOAT:04",instr.ask('MEASUREMENT:MEAS4:VALUE?')) #QUERY SCOPE FOR MEAS 4 - AMPLITUDE OF NEH CAST REF RF, ASSIGN TO PV
+    caput("LAS:CAST:FLOAT:02",instr.ask('MEASUREMENT:MEAS3:VALUE?')) #QUERY SCOPE FOR MEAS 2 - PHASE DIFFERENCE BETWEEN S29 - FEH
+    # caput("LAS:CAST:FLOAT:03",instr.ask('MEASUREMENT:MEAS3:VALUE?')) #QUERY SCOPE FOR MEAS 3 - AMPLITUDE OF S29 REF RF, ASSIGN TO PV
+    # caput("LAS:CAST:FLOAT:04",instr.ask('MEASUREMENT:MEAS4:VALUE?')) #QUERY SCOPE FOR MEAS 4 - AMPLITUDE OF NEH CAST REF RF, ASSIGN TO PV
 
     time.sleep(0.1) # update rate to 10 Hz 
-    
-# use bash command "nohup python run.py" to keep it running in the background
